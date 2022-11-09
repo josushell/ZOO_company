8b21f42d666cf0e17ecf33c9e2bd81bf1ff175b2
josu_shell <yoy07030@khu.ac.kr>
2022-11-09 14:43:53 +0900

    [New] 추천서 view controller

---
+++ References/ReferenceLayout.swift
@@ -0,0 +1,14 @@
+//
+//  ReferenceLayout.swift
+//  Zoo Company
+//
+//  Created by JOSUEYEON on 2022/11/09.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Reference layout:
+class layout_reference {
+    
+}
---
+++ References/ReferenceViewControllerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ResultViewController.swift
+//  Zoo Company
+//
+//  Created by JOSUEYEON on 2022/11/09.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - 추천서 페이지 & 사원증
+class ReferenceViewController: BaseViewController {
+    
+}
