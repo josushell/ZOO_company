fde7604e66fcee0188bcd93b689df0ac7fd013f8
josushell <yoy07030@khu.ac.kr>
2022-11-12 14:16:50 +0900

    [New] CoreML 모델 학습 및 반입

---
+++ References/MLResult.swift
@@ -0,0 +1,38 @@
+//
+//  MLResult.swift
+//  Zoo Company
+//
+//  Created by JOSUEYEON on 2022/11/12.
+//
+
+import Foundation
+import UIKit
+import CoreML
+
+// coreML classification ML import
+class MLResult {
+    let config = MLModelConfiguration()
+    var model: ResultDeptClassification?
+    var result: [Double] = []
+    
+    func predictDept() -> Int {
+        do {
+            model = try ResultDeptClassification(configuration: config)
+        }
+        catch {
+            // todo: error 처리
+        }
+        
+        // 사용자 선택지 가져오기
+        if let appdel = UIApplication.shared.delegate as? AppDelegate {
+            self.result = appdel.GameStat
+        }
+        
+        // ML 예측
+        guard let predict = try? model?.prediction(M: result[GameStats.Mental.rawValue], r: result[GameStats.Relationship.rawValue], d: result[GameStats.Diligent.rawValue], e: result[GameStats.Efficiency.rawValue], p: result[GameStats.Passion.rawValue]) else {
+            return -1 // error
+        }
+        
+        return predict.dept.hashValue
+    }
+}
